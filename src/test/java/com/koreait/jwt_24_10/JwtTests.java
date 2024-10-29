96c61c8a7efd719d8d57ee4f9c29973f2ddbe188
JerryKiki <gasi758@gmail.com>
2024-10-29 10:41:34 +0900

    테스트 3, 4번 추가

--- src/test/java/com/koreait/jwt_24_10/JwtTests.java
+++ src/test/java/com/koreait/jwt_24_10/JwtTests.java
@@ -1,8 +1,10 @@
 package com.koreait.jwt_24_10;
 
+import com.koreait.jwt_24_10.base.jwt.JwtProvider;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import javax.crypto.SecretKey;
@@ -10,13 +12,19 @@ import java.util.Base64;
 import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class JwtTests {
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
     @Value("${custom.jwt.secretKey}")
     private String secretKeyPlain;
+
     @Test
     @DisplayName("secretKey가 존재 해야함")
     void t1() {
         assertThat(secretKeyPlain).isNotNull();
     }
+
     @Test
     @DisplayName("secretKey 원문으로 hmac 암호화 알고리즘에 맞는 SecretKey 객체를 만들 수 있다.")
     void t2() {
@@ -24,6 +32,23 @@ class JwtTests {
         String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
         // Base64 인코딩 된 키를 이용해서 SecretKey 객체를 만든다.
         SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
-        assertThat(secretKeyPlain).isNotNull();
+        assertThat(secretKey).isNotNull();
+    }
+
+    @Test
+    @DisplayName("JwtProvider 객체로 SecretKey 객체를 생성한다.")
+    void t3() {
+        SecretKey secretKey = jwtProvider.getSecretKey();
+
+        assertThat(secretKey).isNotNull();
+    }
+
+    @Test
+    @DisplayName("SecretKey 객체는 단 한번만 생성되어야 함.")
+    void t4() {
+        SecretKey secretKey1 = jwtProvider.getSecretKey();
+        SecretKey secretKey2 = jwtProvider.getSecretKey();
+
+        assertThat(secretKey1 == secretKey2).isTrue();
     }
 }
